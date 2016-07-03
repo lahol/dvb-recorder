7af2b376dc8dd47828b83583fccc6441a8a77028
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2016-07-03 08:44:05 +0200

    Make record status format configurable. Update TODO.

--- main.c
+++ main.c
@@ -64,6 +64,8 @@ struct {
     GTimer *hide_cursor_timer;
     guint record_status_update_source;
     GdkCursor *blank_cursor;
+
+    gchar *rec_status_format;
 } appdata;
 
 AppStatus appstatus;
@@ -411,12 +413,35 @@ gboolean main_update_record_status(gpointer userdata)
     gchar tbuf[256];
     gchar *markup;
     util_duration_to_string(tbuf, 256, recstatus.elapsed_time);
-    if (recstatus.status == DVB_RECORD_STATUS_RECORDING) {
-        markup = g_markup_printf_escaped("<span size=\"48000\" color=\"red\">R: %s</span>", tbuf);
+
+    if (G_UNLIKELY(appdata.rec_status_format == NULL)) {
+        gchar *font = NULL;
+        gchar *color = NULL;
+        gchar *font_format;
+        gchar *color_format;
+        if (config_get("main", "record-status-font", CFG_TYPE_STRING, &font) != 0) {
+            font_format = g_strdup("size=\"48000\"");
+        }
+        else {
+            font_format = g_strdup_printf("font=\"%s\"", font);
+            g_free(font);
+        }
+        if (config_get("main", "record-status-color", CFG_TYPE_STRING, &color) != 0) {
+            color_format = g_strdup("color=\"red\"");
+        }
+        else {
+            color_format = g_strdup_printf("color=\"%s\"", color);
+            g_free(color);
+        }
+
+        appdata.rec_status_format = g_strdup_printf("<span %s %s>%%s: %%s</span>",
+                                                    font_format, color_format);
+        g_free(font_format);
+        g_free(color_format);
     }
-    else {
-        markup = g_markup_printf_escaped("<span size=\"48000\" color=\"red\">S: %s</span>", tbuf);
-    }
+
+    markup = g_markup_printf_escaped(appdata.rec_status_format,
+            recstatus.status == DVB_RECORD_STATUS_RECORDING ? "R" : "S", tbuf);
 
     gtk_label_set_markup(GTK_LABEL(widgets.status_label), markup);
     g_free(markup);
@@ -1017,6 +1042,8 @@ int main(int argc, char **argv)
         g_source_remove(appdata.hide_cursor_source);
     if (appdata.blank_cursor)
         g_object_unref(G_OBJECT(appdata.blank_cursor));
+    if (appdata.rec_status_format)
+        g_free(appdata.rec_status_format);
 
 
     LOG("done\n");
