450534c4caa69678a349cee6d7fcff03eb776968
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2021-02-05 21:55:54 +0100

    Remove deprecated warnings: EpgEventWidget.

--- epg-event-widget.c
+++ epg-event-widget.c
@@ -3,7 +3,7 @@
 #include <glib/gprintf.h>
 #include <time.h>
 
-struct _EpgEventWidgetPrivate {
+typedef struct _EpgEventWidgetPrivate {
     /* private data */
     GtkWidget *expander;
     GtkWidget *short_desc;
@@ -11,9 +11,9 @@ struct _EpgEventWidgetPrivate {
     GtkWidget *items_list;
 
     EPGEvent *event;
-};
+} EpgEventWidgetPrivate;
 
-G_DEFINE_TYPE(EpgEventWidget, epg_event_widget, GTK_TYPE_BIN);
+G_DEFINE_TYPE_WITH_PRIVATE(EpgEventWidget, epg_event_widget, GTK_TYPE_BIN);
 
 enum {
     PROP_0,
@@ -24,8 +24,9 @@ enum {
 static void epg_event_widget_dispose(GObject *gobject)
 {
     EpgEventWidget *self = EPG_EVENT_WIDGET(gobject);
-    epg_event_free(self->priv->event);
-    self->priv->event = NULL;
+    EpgEventWidgetPrivate *priv = epg_event_widget_get_instance_private(self);
+    epg_event_free(priv->event);
+    priv->event = NULL;
 
     G_OBJECT_CLASS(epg_event_widget_parent_class)->dispose(gobject);
 }
@@ -37,7 +38,7 @@ static void epg_event_widget_finalize(GObject *gobject)
     G_OBJECT_CLASS(epg_event_widget_parent_class)->finalize(gobject);
 }
 
-static void epg_event_widget_set_property(GObject *object, guint prop_id, 
+static void epg_event_widget_set_property(GObject *object, guint prop_id,
         const GValue *value, GParamSpec *spec)
 {
     EpgEventWidget *self = EPG_EVENT_WIDGET(object);
@@ -56,10 +57,11 @@ static void epg_event_widget_get_property(GObject *object, guint prop_id,
         GValue *value, GParamSpec *spec)
 {
     EpgEventWidget *self = EPG_EVENT_WIDGET(object);
+    EpgEventWidgetPrivate *priv = epg_event_widget_get_instance_private(self);
 
     switch (prop_id) {
     	case PROP_EVENT:
-            g_value_set_object(value, self->priv->event);
+            g_value_set_object(value, priv->event);
             break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, spec);
@@ -89,58 +91,57 @@ static void epg_event_widget_class_init(EpgEventWidgetClass *klass)
 
 void epg_event_widget_update_event(EpgEventWidget *self)
 {
-    if (self->priv->event == NULL)
+    EpgEventWidgetPrivate *priv = epg_event_widget_get_instance_private(self);
+    if (priv->event == NULL)
         return;
 
     gchar *buf;
     gchar tbuf[64];
     struct tm *tm;
     /* -> lib: get descriptor for preffered language */
-    EPGShortEvent *se = self->priv->event->short_descriptions ?
-                        (EPGShortEvent *)self->priv->event->short_descriptions->data : NULL;
-    EPGExtendedEvent *ee = self->priv->event->extended_descriptions ?
-                           (EPGExtendedEvent *)self->priv->event->extended_descriptions->data : NULL;
-    if (self->priv->expander) {
-        tm = localtime(&self->priv->event->starttime);
+    EPGShortEvent *se = priv->event->short_descriptions ?
+                        (EPGShortEvent *)priv->event->short_descriptions->data : NULL;
+    EPGExtendedEvent *ee = priv->event->extended_descriptions ?
+                           (EPGExtendedEvent *)priv->event->extended_descriptions->data : NULL;
+    if (priv->expander) {
+        tm = localtime(&priv->event->starttime);
         strftime(tbuf, 64, "%d.%m. %H:%M", tm);
         fprintf(stderr, "se->desc: %s\n", se ? se->description : NULL);
         buf = g_strdup_printf("%s <b>%s</b>", tbuf, se ? se->description : "<i>no title</i>");
-        gtk_expander_set_label(GTK_EXPANDER(self->priv->expander), buf);
+        gtk_expander_set_label(GTK_EXPANDER(priv->expander), buf);
     }
-    if (self->priv->short_desc) {
-        gtk_label_set_markup(GTK_LABEL(self->priv->short_desc), se ? se->text : "<i>no description</i>");
+    if (priv->short_desc) {
+        gtk_label_set_markup(GTK_LABEL(priv->short_desc), se ? se->text : "<i>no description</i>");
     }
-    if (self->priv->extended_desc) {
-        gtk_label_set_markup(GTK_LABEL(self->priv->extended_desc), ee ? ee->text : "<i>no description</i>");
+    if (priv->extended_desc) {
+        gtk_label_set_markup(GTK_LABEL(priv->extended_desc), ee ? ee->text : "<i>no description</i>");
     }
 }
 
 static void populate_widget(EpgEventWidget *self)
 {
-    self->priv->expander = gtk_expander_new("HH:MM Event title");
-    gtk_expander_set_use_markup(GTK_EXPANDER(self->priv->expander), TRUE);
+    EpgEventWidgetPrivate *priv = epg_event_widget_get_instance_private(self);
+    priv->expander = gtk_expander_new("HH:MM Event title");
+    gtk_expander_set_use_markup(GTK_EXPANDER(priv->expander), TRUE);
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 1);
 
-    self->priv->short_desc = gtk_label_new("Short Description");
-    gtk_widget_set_halign(self->priv->short_desc, GTK_ALIGN_START);
-    gtk_box_pack_start(GTK_BOX(vbox), self->priv->short_desc, FALSE, FALSE, 2);
+    priv->short_desc = gtk_label_new("Short Description");
+    gtk_widget_set_halign(priv->short_desc, GTK_ALIGN_START);
+    gtk_box_pack_start(GTK_BOX(vbox), priv->short_desc, FALSE, FALSE, 2);
 
-    self->priv->extended_desc = gtk_label_new("Some longer description");
-    gtk_widget_set_halign(self->priv->extended_desc, GTK_ALIGN_START);
-    gtk_box_pack_start(GTK_BOX(vbox), self->priv->extended_desc, FALSE, FALSE, 2);
+    priv->extended_desc = gtk_label_new("Some longer description");
+    gtk_widget_set_halign(priv->extended_desc, GTK_ALIGN_START);
+    gtk_box_pack_start(GTK_BOX(vbox), priv->extended_desc, FALSE, FALSE, 2);
 
-    gtk_container_add(GTK_CONTAINER(self->priv->expander), vbox);
+    gtk_container_add(GTK_CONTAINER(priv->expander), vbox);
 
-    gtk_container_add(GTK_CONTAINER(self), self->priv->expander);
+    gtk_container_add(GTK_CONTAINER(self), priv->expander);
 
     epg_event_widget_update_event(self);
 }
 
 static void epg_event_widget_init(EpgEventWidget *self)
 {
-    self->priv = G_TYPE_INSTANCE_GET_PRIVATE(self,
-            EPG_EVENT_WIDGET_TYPE, EpgEventWidgetPrivate);
-
     gtk_widget_set_has_window(GTK_WIDGET(self), FALSE);
 
     populate_widget(self);
@@ -154,10 +155,11 @@ GtkWidget *epg_event_widget_new(EPGEvent *event)
 void epg_event_widget_set_event(EpgEventWidget *widget, EPGEvent *event)
 {
     g_return_if_fail(IS_EPG_EVENT_WIDGET(widget));
+    EpgEventWidgetPrivate *priv = epg_event_widget_get_instance_private(widget);
 
-    if (widget->priv->event != event)
-        epg_event_free(widget->priv->event);
-    widget->priv->event = epg_event_dup(event);
+    if (priv->event != event)
+        epg_event_free(priv->event);
+    priv->event = epg_event_dup(event);
 
     epg_event_widget_update_event(widget);
 }
@@ -165,7 +167,8 @@ void epg_event_widget_set_event(EpgEventWidget *widget, EPGEvent *event)
 EPGEvent *epg_event_widget_get_event(EpgEventWidget *widget)
 {
     g_return_val_if_fail(IS_EPG_EVENT_WIDGET(widget), NULL);
+    EpgEventWidgetPrivate *priv = epg_event_widget_get_instance_private(widget);
 
-    return widget->priv->event;
+    return priv->event;
 }
 
