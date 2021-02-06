f005084bc2b7195bcdf67cbdccd9e652c556a429
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2021-02-06 11:49:00 +0100

    Set tag and file/line before log lines.

--- logging.h
+++ logging.h
@@ -5,4 +5,4 @@
 void log_log(gchar *format, ...);
 void log_logger_cb(gchar *buffer, gpointer userdata);
 
-#define LOG(fmt, ...) log_log(fmt, ##__VA_ARGS__)
+#define LOG(fmt, ...) log_log("[dvb-recorder] " __FILE__ ":%d " fmt, __LINE__, ##__VA_ARGS__)
